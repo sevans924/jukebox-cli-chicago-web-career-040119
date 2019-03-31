dba4c849cf2a82adeaca5f4481c9b961d0153c99
Sarah Elzinga <sevans924@gmail.com>
2019-03-31 23:40:25 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -2,25 +2,33 @@
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
 
-# my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
-# }
+ my_songs = {
+ "Go Go GO" => '/home/exuberant-scraper-7780/jukebox-cli/audio/Emerald-Park/01.mp3',
+ "LiberTeens" => '/home/exuberant-scraper-7780/jukebox-cli/audio/Emerald-Park/02.mp3',
+ "Hamburg" =>  '/home/exuberant-scraper-7780/jukebox-cli/audio/Emerald-Park/03.mp3',
+ "Guiding Light" => '/home/exuberant-scraper-7780/jukebox-cli/audio/Emerald-Park/04.mp3',
+ "Wolf" => '/home/exuberant-scraper-7780/jukebox-cli/audio/Emerald-Park/05.mp3',
+ "Blue" => '/home/exuberant-scraper-7780/jukebox-cli/audio/Emerald-Park/06.mp3',
+ "Graduation Failed" => '/home/exuberant-scraper-7780/jukebox-cli/audio/Emerald-Park/07.mp3'
+ }
 
 def help
-  #this method should be the same as in jukebox.rb
+  help = "I accept the following commands:
+  - help : displays this help message
+  - list : displays a list of songs you can play
+  - play : lets you choose a song to play
+  - exit : exits this program"
+
+  puts help#this method should be the same as in jukebox.rb
 
 end
 
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
+  my_songs.keys.each_with_index do |song, index|
+    puts "#{index}. #{song}"
+  end#this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
 end
 
@@ -33,7 +41,7 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+
 end
 
 def exit_jukebox
